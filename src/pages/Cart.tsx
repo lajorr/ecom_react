2c2da3ab39e5771c8202775aba893fa63ee31434
lajor <rojal.lajor@gmail.com>
2025-01-03 21:24:04 +0545

    update and remove cart items

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,3 +1,6 @@
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 import { useCartContext } from "../providers/CartContext";
 
 const Cart = () => {
@@ -5,25 +8,42 @@ const Cart = () => {
     const cartContext = useCartContext();
     console.log(cartContext.orderLength);
 
-    const columnNames = ["Product", "Quantity", "Sub total"];
+    const columnNames = ["Product", "Price", "Quantity", "Sub total"];
     return (
         <div className="mt-12 ">
-            <div className="grid grid-cols-3  items-center text-[16px] py-3 px-4 mb-5 bg-gray-200" >
+            <div className="grid grid-cols-4 items-center text-[16px] py-3 px-4 mb-5 bg-gray-200" >
                 {columnNames.map((name, idx) => (
-                    <div key={name} className={`font-[500] ${idx === 1 && "place-self-center"} `}>{name}</div>
+                    <div key={name} className={`${idx !== 0 && "place-self-end"}`}>{name}</div>
                 ))}
             </div>
             <div className="flex flex-col gap-4">
                 {cartContext.orderLength > 0 &&
 
                     cartContext.orderList.map(order => (
-                        <div key={order.product.id} className="grid grid-cols-3 gap-2 items-center text-[16px]" >
-                            <div className="flex items-center gap-2">
-                                <img className="size-16 object-cover rounded-[4px]" src={order.product.product_image} alt="" />
-                                <div className="font-[500] ">{order.product.product_name}</div>
+                        <div className="relative px-6" >
+                            <button className="absolute size-6 top-[-8px] left-4 bg-red-500 rounded-full flex justify-center items-center" onClick={() => { cartContext.removeItemFromCart(order.product.id) }} >
+                                <CloseRoundedIcon fontSize='small' sx={{ color: "white" }} />
+                            </button>
+                            <div key={order.product.id} className="grid grid-cols-4 gap-2 items-center place-items-end text-[16px]" >
+                                <div className="flex items-center gap-2 place-self-start">
+                                    <img className="size-16 object-cover rounded-[4px]" src={order.product.product_image} alt="" />
+                                    <div className="font-[500] ">{order.product.product_name}</div>
+                                </div>
+                                <div>${order.product.product_price.toFixed(2)}</div>
+                                <div className=" border flex gap-2" >
+                                    <button className='bg-green-400 rounded-[4px]'
+                                        onClick={() => cartContext.incrementQuantity(order.product.id)}>
+                                        <AddRoundedIcon sx={{ color: "white" }} />
+                                    </button>
+                                    {order.quantity}
+                                    <button className='bg-red-500 rounded-[4px]'
+                                        onClick={() => cartContext.decrementQuantity(order.product.id)}>
+                                        <RemoveRoundedIcon sx={{ color: "white" }} />
+                                    </button>
+                                </div>
+                                <div>${order.subTotal.toFixed(2)}</div>
                             </div>
-                            <div className="place-self-center" >{order.quantity}</div>
-                            <div>${order.subTotal.toFixed(2)}</div>
+                            <hr className='mt-4 h-0.5 bg-black/30 ' />
                         </div>
                     ))
 
@@ -32,16 +52,18 @@ const Cart = () => {
 
             </div>
             <div className=" flex justify-end mt-20 ">
-                <div className="bg-gray-200 rounded-[4px] px-4 py-4 flex flex-col gap-4">
+                <div className="bg-gray-200 rounded-[4px] px-4 py-4 flex flex-col gap-4 w-[300px]">
                     <CartPricings title="Sub Total" value={`$${cartContext.getTotal()}`} />
+                    <hr className="bg-black h-0.5" />
                     <CartPricings title="Shipping Fee" value="Free" />
+                    <hr className="bg-black h-0.5" />
                     <CartPricings title="Total" value={`$${cartContext.getTotal()}`} />
                     <button className={`${cartContext.orderLength === 0 ? "bg-gray-500 cursor-not-allowed" : "bg-green-600"} text-white py-2 rounded-[4px]`} onClick={() => {
                         console.log("Checkout");
                     }} >Checkout</button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
@@ -52,9 +74,10 @@ type CartPricingsType = {
 }
 const CartPricings = ({ title, value }: CartPricingsType) => {
     return (
-        <p className="text-[14px] grid grid-cols-2 gap-4 ">
+        <p className="text-[14px] flex justify-between">
             <span className="font-bold" >{title}:</span>{value}
-        </p>)
+        </p>
+    )
 }
 
 export default Cart
--- src/providers/CartContext.tsx
+++ src/providers/CartContext.tsx
@@ -9,9 +9,12 @@ type Order = {
 
 type CartContextType = {
     orderList: Order[],
-    addToCart: (productZ: Product) => void,
+    addToCart: (product: Product) => void,
     orderLength: number,
-    getTotal: () => number
+    getTotal: () => number,
+    removeItemFromCart: (id: number) => void,
+    incrementQuantity: (id: number) => void,
+    decrementQuantity: (id: number) => void,
 
 }
 
@@ -20,35 +23,65 @@ const CartContext = createContext<CartContextType | undefined>(undefined);
 export const CartProvider = ({ children }: { children: ReactNode }) => {
     const [orders, setOrders] = useState<Order[]>([]);
 
-
-
     const calculateTotal = () => {
         const total = orders.reduce((prev, current) => prev + current.subTotal, 0).toFixed(2)
         return Number(total);
     }
 
+    const removeItem = (id: number) => {
+        const updatedCart = orders.filter(order => order.product.id !== id);
+        setOrders(updatedCart);
+    }
+
     const addToCart = (product: Product) => {
-        console.log("add to cart function");
         const newOrder: Order = { product, quantity: 1, subTotal: product.product_price }
         const existingOrder = orders.find(order => order.product.id === product.id);
 
-        console.log("new Order: ", newOrder);
-        console.log("existing: ", existingOrder);
         if (existingOrder) {
             existingOrder.quantity++;
             existingOrder.subTotal += product.product_price;
-            console.log("cart updated");
         } else {
-            console.log("new order added");
             setOrders(prev => [...prev, newOrder]);
+        }
+    }
+
+    const incrementQuantity = (id: number) => {
+        const order = orders.find(order => order.product.id === id);
+        if (order) {
+            // check if in stock
+            const updatedOrder = { ...order, quantity: order.quantity + 1, subTotal: order.subTotal + order.product.product_price };
+            setOrders(prev => prev.map(order => order.product.id === id ? updatedOrder : order));
         }
 
     }
 
-    return (<CartContext.Provider value={{ orderList: orders, addToCart, orderLength: orders.length, getTotal: calculateTotal }}>
+    const decrementQuantity = (id: number) => {
+        const order = orders.find(order => order.product.id === id);
+        if (order) {
+            if (order.quantity > 1) {
 
-        {children}
-    </CartContext.Provider>)
+                const updatedOrder = { ...order, quantity: order.quantity - 1, subTotal: order.subTotal - order.product.product_price }
+                setOrders(prev => prev.map(order => order.product.id === id ? updatedOrder : order))
+            }
+        }
+
+    }
+
+    return (
+        <CartContext.Provider value={
+            {
+                orderList: orders,
+                addToCart,
+                orderLength: orders.length,
+                getTotal: calculateTotal,
+                removeItemFromCart: removeItem,
+                incrementQuantity,
+                decrementQuantity
+            }
+        }
+        >
+            {children}
+        </CartContext.Provider>)
 }
 
 export const useCartContext = () => {
